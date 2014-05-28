488bf2770c82dae2c3804a86212d5aba583a7a5d
Nathan Matias <natematias@gmail.com>
2014-05-28 12:08:28 -0400

    added favicon

--- server.js
+++ server.js
@@ -40,7 +40,7 @@ app.configure(function () {
     app.set('port', process.env.PORT || 3000);
     app.set('views', __dirname + '/views');
 
-    app.use(express.favicon());
+    app.use(express.favicon(__dirname + "/public/favicon.ico"));
     app.use(express.logger('dev'));
     app.use(express.bodyParser());
     app.use(express.methodOverride());
