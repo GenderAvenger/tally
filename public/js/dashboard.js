99382084b5f738259672be1d895d87b37b2e6c83
slifty <slifty@gmail.com>
2015-07-08 17:58:22 -0400

    basic dashboard finished

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -1,30 +1,146 @@
 $(function() {
 
+  var headTime = Date.now();
+  var tailTime = Date.now();
+
   var renderCharts = function(newCharts) {
-    console.log(newCharts);
+    var keys = Object.keys(newCharts);
+    for(var x in keys) {
+      var chartCode = keys[x];
+      var chart = newCharts[chartCode];
+      if(!chart.hashtag)
+        chart.hashtag = "(none)"; // In case hash isn't set somehow
+
+      var hashCode = chart.hashtag.replace(/[^a-zA-Z0-9]/,"");
+
+      var timestamp = new Date(chart.timestamp);
+      var dateCode = (timestamp.getMonth() + 1) + "-" + timestamp.getDate() + "-" + timestamp.getFullYear()
+
+      // Does this date exist?
+      var $date = $("#date-" + dateCode);
+      if($date.length == 0) {
+        $date = $("<li>")
+          .attr("id", "date-" + dateCode)
+          .addClass("date")
+          .appendTo($("#timeline"));
+
+        $dateTitle = $("<h2>")
+          .text((timestamp.getMonth() + 1) + "/" + timestamp.getDate() + "/" + timestamp.getFullYear())
+          .appendTo($date);
+
+      }
+
+      // Does this hashtag exist for this date?
+      var $hash = $("#hash-" + hashCode + "-" + dateCode);
+      if($hash.length == 0) {
+        $hash = $("<li>")
+          .attr("id", "hash-" + hashCode + "-" + dateCode)
+          .addClass("hash")
+          .appendTo($date);
+
+        $hashTitle = $("<a>")
+          .attr("href", "https://twitter.com/search?q=" + encodeURIComponent(chart.hashtag))
+          .attr("target", "_blank")
+          .addClass("title")
+          .text(chart.hashtag)
+          .appendTo($hash);
+      }
+
+      // Does the chart exist?
+      var $chart = $("#chart-" + chartCode);
+      if($chart.length != 0) {
+        continue;
+      }
+
+      $chart = $("<div>")
+        .attr("id", chartCode)
+        .addClass("chart")
+        .appendTo($hash);
+
+      $chartTitle = $("<h4>")
+        .addClass("chart-title")
+        .text(chart.session_text)
+        .appendTo($chart);
+
+      $chartLink = $("<a>")
+        .attr("href", "http://app.genderavenger.com/plot/" + chartCode)
+        .attr("target", "_blank")
+        .addClass("chart-link")
+        .appendTo($chart);
+
+      $chartImage = $("<img>")
+        .attr("src", chart.pie_url)
+        .addClass("chart-image")
+        .appendTo($chartLink);
+
+      $chartStats = $("<div>")
+        .addClass("chart-stats")
+        .appendTo($chart);
+
+      // Get share counts
+      (function() {
+        var targetChart = chart;
+        var $targetChart = $chart;
+        var $targetStats = $chartStats;
+        $.ajax("https://cdn.api.twitter.com/1/urls/count.json?url=" + encodeURIComponent("http://app.genderavenger.com/plot/" + chartCode), {
+          dataType: "jsonp",
+          method: "GET"
+        }).done(function(data) {
+          var $chartStats_twitter = $("<a>")
+            .attr("href", "https://twitter.com/search?q=" + encodeURIComponent("http://app.genderavenger.com/plot/" + targetChart.pie_id))
+            .attr("target", "_blank")
+            .addClass("twitter")
+            .html(data.count)
+            .appendTo($targetStats)
+          $chart.data("twitter", data.count)
+        })
+      })()
+    }
   }
 
-  var loadCharts = function(modifier, plotId) {
 
-    var url = '/charts';
+  var loadOldActive = false;
+  var loadOld = function() {
+    if(loadOldActive)
+      return;
 
-    switch(modifier) {
-      case 'start':
-        break;
-      case 'before':
-        url += '/before/' + plotId;
-        break;
-      case 'after':
-        url += '/after/' + plotId;
-        break;
-    }
-    $.get(url)
+    // Don't load old things too many times
+    loadOldActive = true;
+
+    var newTailTime = tailTime - 60*60*24*1000*5; // 5 days
+    $.ajax('charts/' + tailTime + "/" + newTailTime,
+      {
+        dataType: "JSON",
+        method: "GET"
+      })
+      .done(function(data) {
+        renderCharts(data);
+        loadOldActive = false;
+        checkScroll();
+      });
+    tailTime = newTailTime;
+  }
+
+  var loadNew = function() {
+    var newHeadTime = Date.now();
+    $.get('charts/' + newHeadTime + "/" + headTime)
       .done(function(data) {
         renderCharts(data);
       });
+    headTime = newHeadTime;
 
   }
 
-  loadCharts('start')
+  setInterval(loadNew, 30000);
+  //setInterval(loadOld, 5000);
+  loadOld();
 
+  $timeline = $("#timeline");
+  var checkScroll = function() {
+    if($timeline[0].scrollWidth - $timeline.scrollLeft() - 1 == $timeline.outerWidth()) {
+      console.log("BOOP");
+      loadOld();
+    }
+  };
+  $timeline.scroll(checkScroll);
 })
--- routes/dashboard.js
+++ routes/dashboard.js
@@ -8,38 +8,39 @@ app.get('/dashboard', function (req, res, next) {
 
 });
 
-// app.get('/charts/:endTime?/:startTime?', function (req, res, next) {
+app.get('/charts/:endTime?/:startTime?', function (req, res, next) {
 
-//   var query = null;
-//   var end = Date.now();
-//   var start = end;
-//   if(req.params.endTime) {
-//     end = req.params.endTime;
-//   }
+  var query = null;
+  var end = Date.now();
+  var start = end;
+  if(req.params.endTime)
+    end = parseInt(req.params.endTime);
 
-//   if(req.params.startTime) {
-//     start = req.params.startTime;
-//   } else {
-//     start = end - 86400 * 5; // get one day by default
-//   }
+  if(req.params.startTime)
+    start = parseInt(req.params.startTime);
+  else
+    start = end - 86400000 * 5; // get a five day window day by default
 
-//   query = firebaseDatastore.child("plots").orderByChild("unicode-timestamp").startAt(start).endAt(end);//.limitToLast(4);
+  console.log (start + ":" + end);
+  firebaseDatastore.child("plots")
+    .startAt(start)
+    .endAt(end)
+    .once("value", function(snapshot) {
+      res.send(JSON.stringify(snapshot.val()));
+    });
+});
 
-//   query.once("value", function(snapshot) {
-//     res.send(JSON.stringify(snapshot.val()));
-//   });
-// });
 
+app.get('/clean', function (req, res, next) {
 
-// app.get('/clean', function (req, res, next) {
+  query = firebaseDatastore.child("plots");
 
-//   query = firebaseDatastore.child("plots");
-
-//   query.on("child_added", function(snapshot) {
-//     var val = snapshot.val();
-//     var d = new Date(val.timestamp);
-//     snapshot.ref().update({
-//       "unicode-timestamp": d.getTime()
-//     })
-//   });
-// });
+  query.on("child_added", function(snapshot) {
+    var val = snapshot.val();
+    var d = new Date(val.timestamp);
+    snapshot.ref().update({
+      "unicode-timestamp": d.getTime()
+    })
+    snapshot.ref().setPriority(d.getTime());
+  });
+});
