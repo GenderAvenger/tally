4fa69d60f20ccee3fa3dbc61e056e498da5a44d4
slifty <slifty@gmail.com>
2015-07-08 14:02:03 -0400

    dash start

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -1,11 +1,30 @@
 $(function() {
 
   var renderCharts = function(newCharts) {
+    console.log(newCharts);
+  }
+
+  var loadCharts = function(modifier, plotId) {
+
+    var url = '/charts';
+
+    switch(modifier) {
+      case 'start':
+        break;
+      case 'before':
+        url += '/before/' + plotId;
+        break;
+      case 'after':
+        url += '/after/' + plotId;
+        break;
+    }
+    $.get(url)
+      .done(function(data) {
+        renderCharts(data);
+      });
 
   }
 
-  var loadCharts = function() {
-    var plotRef =
-  }
+  loadCharts('start')
 
 })
--- routes/dashboard.js
+++ routes/dashboard.js
@@ -4,16 +4,42 @@ var app = require('../server').app,
 app.get('/dashboard', function (req, res, next) {
 
   res.render('dashboard.html', {
-
   });
 
 });
 
-app.get('/charts/:modifier/:date', function (req, res, next) {
+// app.get('/charts/:endTime?/:startTime?', function (req, res, next) {
 
-  firebaseDatastore.child("plots").orderByChild("timestamp").limit(100).on("child_added", function(snapshot) {
-    console.log(snapshot.val());
-  });
-  //  res.send(JSON.stringify(firebaseDatastore.orderByKey('plots')));
+//   var query = null;
+//   var end = Date.now();
+//   var start = end;
+//   if(req.params.endTime) {
+//     end = req.params.endTime;
+//   }
 
-});
+//   if(req.params.startTime) {
+//     start = req.params.startTime;
+//   } else {
+//     start = end - 86400 * 5; // get one day by default
+//   }
+
+//   query = firebaseDatastore.child("plots").orderByChild("unicode-timestamp").startAt(start).endAt(end);//.limitToLast(4);
+
+//   query.once("value", function(snapshot) {
+//     res.send(JSON.stringify(snapshot.val()));
+//   });
+// });
+
+
+// app.get('/clean', function (req, res, next) {
+
+//   query = firebaseDatastore.child("plots");
+
+//   query.on("child_added", function(snapshot) {
+//     var val = snapshot.val();
+//     var d = new Date(val.timestamp);
+//     snapshot.ref().update({
+//       "unicode-timestamp": d.getTime()
+//     })
+//   });
+// });
